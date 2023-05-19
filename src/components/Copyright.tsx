3745974155287f54ea1cf60ca179a7ff2fe81d94
Matheus <matheus_dario@outlook.com>
2023-05-19 16:26:53 -0300

    perf: :zap: added new components

---
+++ src/components/Copyright.tsx
@@ -0,0 +1,7 @@
+export function Copyright() {
+  return (
+    <div className="text-sm leading-relaxed text-gray-200">
+      Feito por Matheus Dário na NLW 🚀
+    </div>
+  )
+}
---
+++ src/components/Hero.tsx
@@ -0,0 +1,27 @@
+import Image from 'next/image'
+import rocketLogo from '../assets/rocket-logo.svg'
+
+export function Hero() {
+  return (
+    <div className="space-y-5">
+      <Image src={rocketLogo} alt="Rocket logo" />
+
+      <div className="max-w-[420] space-y-4">
+        <h1 className="text-5xl font-bold leading-tight text-gray-50">
+          Sua cápsula do tempo
+        </h1>
+        <p className="text-lg leading-relaxed">
+          Colecione momentos marcantes da sua jornada e compartilhe (se quiser)
+          com o mundo!
+        </p>
+      </div>
+
+      <a
+        className="inline-block rounded-full bg-purple-400 px-5 py-3 font-alt text-sm uppercase leading-none text-black transition hover:bg-purple-500"
+        href=""
+      >
+        CADASTRAR LEMBRANÇA
+      </a>
+    </div>
+  )
+}
