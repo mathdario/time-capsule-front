2fca124a37208084a1c31bf283713f2f0235f57d
Matheus <matheus_dario@outlook.com>
2023-05-20 14:31:40 -0300

    feat: :sparkles: created logout route

---
+++ src/app/api/auth/logout/route.ts
@@ -0,0 +1,12 @@
+import { NextRequest, NextResponse } from 'next/server'
+
+export async function GET(request: NextRequest) {
+  const redirectURL = new URL('/', request.url)
+
+  return NextResponse.redirect(redirectURL, {
+    headers: {
+      // maneira para deletar o cookie
+      'Set-Cookie': `token=; Path=/; max-age=0`,
+    },
+  })
+}
