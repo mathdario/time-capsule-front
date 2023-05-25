ea7bc97d83f1030ec92c8b7412bdaa29b4622c5b
Matheus <matheus_dario@outlook.com>
2023-05-24 23:31:24 -0300

    feat: :sparkles: implemented feed page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,39 @@
 import { EmptyMemories } from '@/components/EmptyMemories'
+import { api } from '@/lib/api'
+import { cookies } from 'next/headers'
 
-export default function Home() {
-  return <EmptyMemories />
+interface Memory {
+  id: string
+  coverUrl: string
+  excerpt: string
+  createdAt: string
+}
+
+export default async function Home() {
+  const isAuthenticated = cookies().has('token')
+
+  if (!isAuthenticated) {
+    return <EmptyMemories />
+  }
+
+  const token = cookies().get('token')?.value
+  const response = await api.get('memories', {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  })
+
+  const memories: Memory[] = response.data
+
+  if (memories.length === 0) {
+    return <EmptyMemories />
+  }
+
+  return (
+    <div className="flex flex-col gap-10 p-8">
+      {memories.map((memory) => {
+        return <div key={memory.id} className="space-y-4"></div>
+      })}
+    </div>
+  )
 }
