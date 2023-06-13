ac8b92f7e2d7a70a6a767f44adbc6ec0d561104e
Matheus <matheus_dario@outlook.com>
2023-06-13 16:07:26 -0300

    feat: :sparkles: added read more section and adjusted for just right side scroll

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,8 @@ import { cookies } from 'next/headers'
 import dayjs from 'dayjs'
 import ptBr from 'dayjs/locale/pt-br'
 import Image from 'next/image'
+import Link from 'next/link'
+import { ArrowRight } from 'lucide-react'
 
 dayjs.locale(ptBr)
 
@@ -42,7 +44,24 @@ export default async function Home() {
             <time className="-ml-8 flex items-center gap-2 text-sm text-gray-100 before:h-px before:w-5 before:bg-gray-50">
               {dayjs(memory.createdAt).format('D[ de ]MMMM[, ]YYYY')}
             </time>
-            <Image src={memory.coverUrl} width={592} height={280} alt="" />
+            <Image
+              src={memory.coverUrl}
+              width={592}
+              height={280}
+              className="aspect-video w-full rounded-lg object-cover"
+              alt=""
+            />
+            <p className="text-lg leading-relaxed text-gray-100">
+              {memory.excerpt}
+            </p>
+
+            <Link
+              href={`/memories/${memory.id}`}
+              className="flex items-center gap-2 text-sm text-gray-200 hover:text-gray-100"
+            >
+              Ler mais
+              <ArrowRight className="h-4 w-4" />
+            </Link>
           </div>
         )
       })}
