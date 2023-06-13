85daf42b25cf628046d68e9ed2cfad246770818e
Matheus <matheus_dario@outlook.com>
2023-06-13 15:54:50 -0300

    feat: :sparkles: updated domains

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import { api } from '@/lib/api'
 import { cookies } from 'next/headers'
 import dayjs from 'dayjs'
 import ptBr from 'dayjs/locale/pt-br'
+import Image from 'next/image'
 
 dayjs.locale(ptBr)
 
@@ -41,6 +42,7 @@ export default async function Home() {
             <time className="-ml-8 flex items-center gap-2 text-sm text-gray-100 before:h-px before:w-5 before:bg-gray-50">
               {dayjs(memory.createdAt).format('D[ de ]MMMM[, ]YYYY')}
             </time>
+            <Image src={memory.coverUrl} width={592} height={280} alt="" />
           </div>
         )
       })}
