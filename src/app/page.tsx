8d3cc4df3b0b2296a6b554ebce5581a9964d9509
Matheus <matheus_dario@outlook.com>
2023-06-13 14:29:11 -0300

    feat: :sparkles: something about memories

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -32,7 +32,13 @@ export default async function Home() {
   return (
     <div className="flex flex-col gap-10 p-8">
       {memories.map((memory) => {
-        return <div key={memory.id} className="space-y-4"></div>
+        return (
+          <div key={memory.id} className="space-y-4">
+            <time className="-ml-8 flex items-center gap-2 text-sm text-gray-100 before:h-px before:w-5 before:bg-gray-50">
+              {memory.createdAt}
+            </time>
+          </div>
+        )
       })}
     </div>
   )
